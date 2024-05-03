9cf93c3d086528d550baeed4fc9e3dcb04688727
Giovanni Gonzalez <createdtyler@gmail.com>
2024-05-03 15:28:22 -0400

    created a module (games.py)contains clas Player and a ask_number function

---
+++ customModule/games.py
@@ -0,0 +1,23 @@
+# Games
+# Demonstrates module creation
+
+class Player(object):
+    ''' A player for a game'''
+    def __init__(self, name, score = 0):
+        self.name = name
+        self.score = score
+        
+    def __str__(self):
+        rep = self.name + ':\t' + str(self.score)
+        return rep
+    
+    def ask_yes_no(question):
+        ''' Ask a yes or no question'''
+        response = None
+        while response not in ('y', 'n'):
+            response = input(question).lower()
+        return response
+    
+    if __name__ == '__main__':
+        print('You ran this module directly (and did not \' import \' it.)')
+        input('\n\nPress any key to exit: ')

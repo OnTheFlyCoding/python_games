2d2bc8cfc0cf13a49afc0c691b8159490a947073
Giovanni Gonzalez <createdtyler@gmail.com>
2024-05-03 21:10:31 -0400

    added the skeleton for the classes we will use in thise program. Added new BJ_ classes that override the ones imported.added two properties that will be used to help calculate a value from the Ids of cards in each players hand.

---
+++ theBlackJackGame/blackJack.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3 
+
+# Blackjack 1-7 players
+
+import cards
+import games
+
+class BJ_Card(cards.Card):
+    ''' A Black Jack Card'''
+    #Since the ace is at the [0] of Card.Ranks
+    ACE_VALUE = 1
+    
+    @property
+    def value(self):
+        # BJ_Card.value to be called
+        if self.is_face_up:
+            #Takes the index of the Rank and adds one to give the value
+            #Ex.) '3d', rank =3, index=[2]. value = Ranks[index] +1
+            v = BJ_Card.RANKS.index(self.rank) +1
+            if v > 10:
+                #ex.) 10 of any card as an index of 9, add 1 to get the true value.
+                #If the index of the card object is passed 10
+                v = 10
+                
+        else:
+            #If card is faceing down
+            v = None
+        return v
+    
+class BJ_Deck(cards.Deck):
+    def populate(self):
+        ''' A Blackjack Deck'''
+        for suit in BJ_Card.SUITS:
+            for rank in BJ_Card.RANKS:
+                # instantiate each Card object and add it to the deck of cards.
+                self.cards.append(BJ_Card(rank,suit))
+                
+class BJ_Hand(cards.Hand):
+    #Overrride the __init__ method:
+    def __init__(self, name):
+        super(BJ_Hand, self).__init__()
+        #^ The initalization of the self.cards list
+        self.name = name
+    # Override the __str__ method:
+    def __str__(self):
+        # Add self.name before displaying the auto string associated with the hand object of 
+        # a specific player. The str should print every card ass. with the hand.
+        rep = self.name + ':\t' + super(BJ_Hand,self).__str__()
+        if self.total:
+            rep += '(' + str(self.total) + ')'
+        return rep
+    
+    @property
+    def total(self):
+        #** if a card in the hand has value of None, then total is None **
+        for card in self.cards:
+            #Checks all the cards in the hand prior to calculating a total, based on value prop.
+            if not card.value:
+                #If there is no value property assc. with the object, return None.
+                # If card facing down return none.
+                return None
+            
+        #**Continue code if all cards in a hand are able to be viewd, thus calculated
+        # add up Card values, treat each ace as 1:**
+        t = 0
+        for card in self.cards:
+            t += card.value
+            
+        # Determine if this hand has an Ace:
+        contains_ace = False
+        for card in self.cards:
+            if card.value == BJ_Card.ACE_VALUE:
+                contains_ace = True
+        
+        #**If hand contains an Ace and the total is low enough, treat ace as 11 by adding 10.
+        # since we got to this portion of code, we added 1 for the ace before determing if
+        # we should treat it as 11 or not.
+        # ex.) combination of an ace and 10 would be 11 before this calculation
+        # Treating the ace as an 11 when the other card is already 11, we would auto bust:(**
+        if contains_ace and t <= 11:
+            t += 10
+        return t
+    
+    #Create a method that checks if we busted or not
+    def is_busted(self):
+        return self.total > 21
+    
+class BJ_player(BJ_Hand):
+    ''' A Blackjack Player '''
+    def is_hitting(self):
+        response = games.ask_yes_no(f'\n{self.name}, do you want a hit? (Y/N): ')
+        return response == 'y'
+    
+    def bust(self):
+        print(self.name, 'busts..😐')
+        self.lose()
+        
+    def lose(self):
+        print(self.name, 'Loses :(')
+        
+    def win(self):
+        print(self.name, 'Wins! :)')
+        
+    def push(self):
+        print(self.name, 'Pushes👀')
+        
